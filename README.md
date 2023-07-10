1a2cd14f3ca3a3dbd4363910373567ce5f2bdd9b
Jack Doyle <footyjack@hotmail.com>
2023-07-10 17:37:26 +0100

    First JS file

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # mixed_messages
 Mixed Messages  | Codecademy
+
+# Aims of this project
+==
+
+- Create a random generated message each time a user runs program.
+- Type of message can be any sort of theme.
+- It must contain at least three different types of data.
+
+## How message will be constructed
+==
+
+- Message will be logged to the console.
+- RNG will look at the indexes of arrays specified to return a result.
+- Message will be formatted in string.
+- User input will be four different parameters, in order to randomise as much as possible.
+
+## Type of message
+==
+
+- My idea is to create several arrays containing several arrays for different scenarios.
+- These arrays will be called upon via aforementioned RNG and loops.
+- Based on user's input, params will be (age(num), colour of hair(scale 1-3 dark to brightest), and food (any string))
+- There should be validations in place to cancel function and start again, using typeof to check for errors.
+
+## The scenarios of the message
+==
+
+- First: Using age, this is the beginning of the message, so I want to pre-empt a career that the user might be in. Could be sport if they are young, politics if they are old etc.
+- Second: This is random, however the more bright the colour hair, I want the career to be more interesting.
+- Third: This is also random, and I plan to be funny with this message. It could be anything.
---
+++ first_version.js
@@ -0,0 +1,79 @@
+// Declaring arrays for the message structure
+
+const ageRangeStarterMessage = ["Nice try, that's not a real age, start again please!",
+"You shouldn't be working, so let's instead predict what you WILL be doing. I see your future in...", 
+"I see you're a spring chicken clearly, so I can see you being involved in...", 
+"Age not on your side now I see, but your career is still in...", 
+"Creeping up to retirement age I see. I predict that you're career is still in...", 
+"Why are you still working?! Anyway, I think that it's a certainty that you are still in...",
+ "Twilight years I see. Let's guess what you were doing...ah of course, it was in..."]
+
+ const careerRange = ['Sports',
+'Politics',
+'Film',
+'TV',
+'Politics',
+'Music',
+'Art',
+'Literature',
+'Theatre']
+
+const hairRange = ['You have hair on the darker side, so I am predicting that you are mysterious and a hidden person in that career',
+'Pretty standard hair I would say, you stick to the rules I bet!',
+'Wow, very bright-coloured hair, you must be the loudest and most flamboyant of them all!']
+
+const foodRange = ['Very smelly food, not sure your colleagues will be a fan of that',
+'Nice choice, Gordon Ramsey would be proud!',
+'Hmm I think you can do better than that for food!',
+' OK I guess for a food choice, maybe put something else and I would give you a different answer']
+
+//Setting function to return starter message based on age
+
+function ageRange(age) {
+    let arr = ageRangeStarterMessage; // Assuming ageRangeStarterMessage is defined
+  
+    if (age < 1) {
+      console.log(arr[0]);
+    } else if (age >= 1 && age <= 17) {
+      console.log(arr[1]);
+    } else if (age >= 18 && age <= 39) {
+      console.log(arr[2]);
+    } else if (age >= 40 && age <= 59) {
+      console.log(arr[3]);
+    } else if (age >= 60 && age <= 64) {
+      console.log(arr[4]);
+    } else if (age >= 65 && age <= 79) {
+      console.log(arr[5]);
+    } else if (age >= 80) {
+      console.log(arr[6]);
+    } else {
+      console.log("Something went wrong");
+    }
+  }
+   //Function to choose random career
+
+
+function randomiseCareer () {
+    let arr = careerRange
+console.log(arr[(Math.floor(Math.random() * arr.length))]);
+}
+
+// function to return message based on hair
+
+function hairMessage(string) {
+    let arr = hairRange; 
+  
+    if (string === 'Dark') {
+      console.log(arr[0]);
+    } else if (string === 'Normal') {
+      console.log(arr[1]);
+    } else if (string === 'Bright') {
+      console.log(arr[2]);
+    } else {
+      console.log("Something went wrong");
+    }
+  }
+
+// testing results via console log
+
+console.log(ageRange(78));
